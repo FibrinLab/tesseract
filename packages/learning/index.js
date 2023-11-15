1b41789990202e64e87a77532f35deae608cf138
Akanimoh Osutuk <fibrinlab@gmail.com>
2023-11-15 14:28:23 +0000

    ci: added more workflows

--- packages/learning/index.js
+++
@@ -1,8 +0,0 @@
-const connectButton = document.getElementById("connectButton");
-connectButton.onclick = connect;
-
-async function connect() {
-    if (typeof window.ethereum !== undefined) {
-        await window.ethereum.request({ method: "eth_requestAccounts" })
-    }
-}
